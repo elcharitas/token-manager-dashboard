139035f5fd0f96d2b4e5c89b0ebbd9522a91726c
elcharitas <jonathanirhodia@gmail.com>
2022-07-24 17:32:48 +0100

    feat: add more functions to abi

--- src/utils/abi.js
+++ src/utils/abi.js
@@ -1,7 +1,10 @@
 export const erc20Abi = [
   "function minAmount() public view returns (uint256)",
   "function maxAmount() public view returns (uint256)",
+  "function symbol() public view returns (string)",
+  "function name() public view returns (string)",
   "function initialSupply() public view returns (uint256)",
+  "function totalSupply() public view returns (uint256)",
   "function setLimit(uint256 _minAmount, uint256 _maxAmount) public",
   "function approve(address spender, uint256 amount) public",
   "function transfer(address to, uint256 amount) public",
