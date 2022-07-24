9d1aff4db5ccfca42f49e3786a07f62f226a42ad
elcharitas <jonathanirhodia@gmail.com>
2022-07-24 17:08:23 +0100

    feat: add token abi utils

---
+++ src/utils/abi.js
@@ -0,0 +1,8 @@
+export const erc20Abi = [
+  "function minAmount() public view returns (uint256)",
+  "function maxAmount() public view returns (uint256)",
+  "function initialSupply() public view returns (uint256)",
+  "function setLimit(uint256 _minAmount, uint256 _maxAmount) public",
+  "function approve(address spender, uint256 amount) public",
+  "function transfer(address to, uint256 amount) public",
+];
