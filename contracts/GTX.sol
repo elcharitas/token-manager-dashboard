99bbba4a2f77d70fd0fc3bc7ead579e9ba5c0257
elcharitas <jonathanirhodia@gmail.com>
2022-07-24 17:08:09 +0100

    feat: create GTX contract

---
+++ contracts/GTX.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity <=0.8.14;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract GTX is ERC20, Ownable {
+    uint256 public immutable initialSupply;
+
+    uint256 public minAmount;
+    uint256 public maxAmount;
+
+    constructor(uint256 _initialSupply) ERC20("GTX", "GTX") {
+        initialSupply = _initialSupply;
+        _mint(msg.sender, _initialSupply);
+    }
+
+    function setLimit(uint256 _minAmount, uint256 _maxAmount) external {
+      minAmount = _minAmount;
+      maxAmount = _maxAmount;
+    }
+}
