54412dc70c67682115860db94962c586d8059049
elcharitas <jonathanirhodia@gmail.com>
2022-08-28 10:08:04 +0100

    add feed sources

---
+++ src/utils/feeds.js
@@ -0,0 +1,142 @@
+const dataFeeds = {
+  1: {
+    // 1INCH
+    "0x111111111117dC0aa78b770fA6A738034120C302": {
+      name: "1INCH",
+      feed: "0xc929ad75B72593967DE83E7F7Cda0493458261D9",
+    },
+    // AAVE
+    "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9": {
+      name: "AAVE",
+      feed: "0x547a514d5e3769680Ce22B2361c10Ea13619e8a9",
+    },
+    // BAT
+    "0x0D8775F648430679A709E98d2b0Cb6250d2887EF": {
+      name: "BAT",
+      feed: "0x9441D7556e7820B5ca42082cfa99487D56AcA958",
+    },
+    // BNB
+    "0xB8c77482e45F1F44dE1745F52C74426C631bDD52": {
+      name: "BNB",
+      feed: "0x14e613AC84a31f709eadbdF89C6CC390fDc9540A",
+    },
+    // BTC
+    "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599": {
+      name: "wBTC",
+      feed: "0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c",
+    },
+    // ETH
+    "0x0000000000000000000000000000000000000001": {
+      name: "ETH",
+      feed: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
+    },
+    // LINK
+    "0x514910771AF9Ca656af840dff83E8264EcF986CA": {
+      name: "LINK",
+      feed: "0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c",
+    },
+    // USDC
+    "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": {
+      name: "USDC",
+      feed: "0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6",
+    },
+  },
+  4: {
+    // BAT
+    "0xE43A8b1bfD19721E918b6068c158bf8faB41aBee": {
+      name: "BAT",
+      feed: "0x031dB56e01f82f20803059331DC6bEe9b17F7fC9",
+    },
+    // BNB
+    "0x8c63C77BC1742f7c03074C25c0AB5758D47e534C": {
+      name: "BNB",
+      feed: "0xcf0f51ca2cDAecb464eeE4227f5295F2384F84ED",
+    },
+    // BTC
+    "0x5AF59f281B3cFd0C12770E4633e6C16dd08eA543": {
+      name: "BTC",
+      feed: "0xECe365B379E1dD183B20fc5f022230C044d51404",
+    },
+    // LINK
+    "0x01BE23585060835E02B77ef475b0Cc51aA1e0709": {
+      name: "LINK",
+      feed: "0xd8bD0a1cB028a31AA859A21A3758685a95dE4623",
+    },
+    // USDC
+    "0x0D1bb5e160f9046a4951B435E57c5B2E8EEd5775": {
+      name: "USDC",
+      feed: "0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB",
+    },
+  },
+  5: {
+    // LINK
+    "0x326C977E6efc84E512bB9C30f76E30c160eD06FB": {
+      name: "LINK",
+      feed: "0x48731cF7e84dc94C5f84577882c14Be11a5B7456",
+    },
+    // BTC
+    "0x28F3BbbF7591617D7162B832468718f6A0dEb43b": {
+      name: "wBTC",
+      feed: "0xA39434A63A52E749F02807ae27335515BA4b07F7",
+    },
+  },
+  137: {
+    // 1INCH
+    "0x9c2C5fd7b07E95EE044DDeba0E97a665F142394f": {
+      name: "1INCH",
+      feed: "0x443C5116CdF663Eb387e72C688D276e702135C87",
+    },
+    // AAVE
+    "0xD6DF932A45C0f255f85145f286eA0b292B21C90B": {
+      name: "AAVE",
+      feed: "0x72484B12719E23115761D5DA1646945632979bB6",
+    },
+    // BAT
+    "0x3Cef98bb43d732E2F285eE605a8158cDE967D219": {
+      name: "BAT",
+      feed: "0x2346Ce62bd732c62618944E51cbFa09D985d86D2",
+    },
+    // BNB
+    "0x3BA4c387f786bFEE076A58914F5Bd38d668B42c3": {
+      name: "BNB",
+      feed: "0x82a6c4AF830caa6c97bb504425f6A66165C2c26e",
+    },
+    // BTC
+    "0x5d49c278340655b56609fdf8976eb0612af3a0c3": {
+      name: "wBTC",
+      feed: "0xc907E116054Ad103354f2D350FD2514433D57F6f",
+    },
+    // LINK
+    "0xb0897686c545045afc77cf20ec7a532e3120e0f1": {
+      name: "LINK",
+      feed: "0xd9FFdb71EbE7496cC440152d43986Aae0AB76665",
+    },
+    // MATIC
+    "0x0000000000000000000000000000000000001010": {
+      name: "MATIC",
+      feed: "0xAB594600376Ec9fD91F8e885dADF0CE036862dE0",
+    },
+    // USDC
+    "0x2791bca1f2de4661ed88a30c99a7a9449aa84174": {
+      name: "USDC",
+      feed: "0xfE4A8cc5b5B2366C1B58Bea3858e81843581b2F7",
+    },
+  },
+  80001: {
+    // BTC
+    "0x5AF59f281B3cFd0C12770E4633e6C16dd08eA543": {
+      name: "BTC",
+      feed: "0x007A22900a3B98143368Bd5906f8E17e9867581b",
+    },
+    // MATIC
+    "0x0000000000000000000000000000000000001010": {
+      name: "BNB",
+      feed: "0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada",
+    },
+    // USDC
+    "0x0D1bb5e160f9046a4951B435E57c5B2E8EEd5775": {
+      name: "USDC",
+      feed: "0x572dDec9087154dC5dfBB1546Bb62713147e0Ab0",
+    },
+  },
+};
